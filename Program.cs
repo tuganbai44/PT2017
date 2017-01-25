aa77ce6b473ebb11e8c097528210bdcbfeb5e1f3
PC44 <PC44>
2017-01-25 11:55:32 +0600

    My First C

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static bool check (int x)
+        {
+            for (int i = 2; i < x; ++i)
+                if (x % i == 0)
+                    return false;
+            if (x == 1)
+                return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                int p = int.Parse(s);
+                if (check(p))
+                    Console.WriteLine(p);
+            }
+        }
+    }
+}
