a69b6d084060369798eccf654b34991c1e4d2055
PC44 <PC44>
2017-01-29 19:46:44 +0600

    Lab2_2

---
+++ Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace Student
+{
+    class MainClass
+    {
+        static void MaxMin()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\user\Documents\Visual Studio 2015\PT2017\Lab2_2\Lab2_2\input.txt");
+            string[] arr = sr.ReadLine().Split();
+            int x = 9999, y = 0;
+            foreach (string s in arr)
+            {
+                int p = int.Parse(s);
+                if (p < x)
+                    x = p;
+                if (p > y)
+                    y = p;
+            }
+            Console.WriteLine(x + " " + y);
+            sr.Close();
+        }
+        static void Main(string[] args)
+        {
+            MaxMin();
+        }
+    }
+}
